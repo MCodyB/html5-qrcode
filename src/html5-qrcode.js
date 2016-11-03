1a79a810441a7b2adf9cc0791ad934b280e506fa
Michael Bertolino <mcodyb@gmail.com>
2016-11-03 16:15:58 -0400

    just needs android testing

--- src/html5-qrcode.js
+++ src/html5-qrcode.js
@@ -1,76 +1,87 @@
 (function($) {
-    jQuery.fn.extend({
-        html5_qrcode: function(qrcodeSuccess, qrcodeError, videoError) {
-            return this.each(function() {
-                var currentElem = $(this);
+  jQuery.fn.extend({
+    html5_qrcode: function(qrcodeSuccess, qrcodeError, videoError) {
+      return this.each(function() {
+          var currentElem = $(this);
 
-                var height = currentElem.height();
-                var width = currentElem.width();
+          var height = currentElem.height();
+          var width = currentElem.width();
 
-                if (height == null) {
-                    height = 250;
-                }
+          if (height == null) {
+              height = 250;
+          }
 
-                if (width == null) {
-                    width = 300;
-                }
+          if (width == null) {
+              width = 300;
+          }
 
-                var vidElem = $('<video width="' + width + 'px" height="' + height + 'px"></video>').appendTo(currentElem);
-                var canvasElem = $('<canvas id="qr-canvas" width="' + (width - 2) + 'px" height="' + (height - 2) + 'px" style="display:none;"></canvas>').appendTo(currentElem);
+          var vidElem = $('<video width="' + width + 'px" height="' + height + 'px"></video>').appendTo(currentElem);
+          var canvasElem = $('<canvas id="qr-canvas" width="' + (width - 2) + 'px" height="' + (height - 2) + 'px" style="display:none;"></canvas>').appendTo(currentElem);
 
-                var video = vidElem[0];
-                var canvas = canvasElem[0];
-                var context = canvas.getContext('2d');
-                var localMediaStream;
+          var video = vidElem[0];
+          var canvas = canvasElem[0];
+          var context = canvas.getContext('2d');
+          var localMediaStream;
 
-                var scan = function() {
-                  if (localMediaStream) {
-                    context.drawImage(video, 0, 0, 307, 250);
+          var scan = function() {
+            if (localMediaStream) {
+              context.drawImage(video, 0, 0, 307, 250);
 
-                    try {
-                      qrcode.decode();
-                    } catch (e) {
-                      qrcodeError(e, localMediaStream);
-                    }
-                  }
-                };//end snapshot function
+              try {
+                qrcode.decode();
+              } catch (e) {
+                qrcodeError(e, localMediaStream);
+              }
+            }
+          };//end snapshot function
 
-                window.URL = window.URL || window.webkitURL || window.mozURL || window.msURL;
-                navigator.getUserMedia = (navigator.mediaDevices || navigator).getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+          window.URL = window.URL || window.webkitURL || window.mozURL || window.msURL;
 
-                var successCallback = function(stream) {
-                    video.src = (window.URL && window.URL.createObjectURL(stream)) || stream;
-                    localMediaStream = stream;
-                    $.data(currentElem[0], "stream", stream);
+          var successCallback = function(stream) {
+              video.src = (window.URL && window.URL.createObjectURL(stream)) || stream;
+              localMediaStream = stream;
+              $.data(currentElem[0], "stream", stream);
 
-                    video.play();
-                    $.data(currentElem[0], "timeout", setInterval(scan, 500));
-                };
+              video.play();
+              $.data(currentElem[0], "timeout", setInterval(scan, 500));
+          };
 
-                // Call the getUserMedia method with our callback functions
-                if (navigator.getUserMedia) {
-                    navigator.getUserMedia({video: true}, successCallback, function(error) {
-                        videoError(error, localMediaStream);
-                    });
-                } else {
-                    console.log('Native web camera streaming (getUserMedia) not supported in this browser.');
-                    // Display a friendly "sorry" message to the user
-                }
+          // Call the getUserMedia method with our callback functions
+          if (navigator.mediaDevices === undefined) navigator.mediaDevices = {};
+          navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
 
-                qrcode.callback = function (result) {
-                    qrcodeSuccess(result, localMediaStream);
-                };
-            }); // end of html5_qrcode
-        },
-        html5_qrcode_stop: function() {
-            return this.each(function() {
-                //stop the stream and cancel timeouts
-                $(this).data('stream').getVideoTracks().forEach(function(videoTrack) {
-                    videoTrack.stop();
-                });
-
-                clearInterval($(this).data('timeout'));
+          // Mozilla suggests using a `Promise` to construct
+          // `navigator.mediaDevices.getUserMedia` out of `navigator.getUserMedia`
+          // But, this seems much simpler and more readable, in the same # lines of code.
+          if (navigator.mediaDevices.getUserMedia) {
+            navigator.mediaDevices.getUserMedia({video: true}).
+            then(successCallback).catch(function(error) {
+              videoError(error, localMediaStream);
             });
-        }
-    });
+          } else if (navigator.getUserMedia) {
+            navigator.getUserMedia({video: true}, successCallback, function(error) {
+                videoError(error, localMediaStream);
+            });
+          } else {
+            console.log('Native web camera streaming (getUserMedia) not supported in this browser.');
+            // Display a friendly "sorry" message to the user
+          }
+          console.log("ber");
+          qrcode.callback = function (result) {
+              qrcodeSuccess(result, localMediaStream);
+          };
+      }); // end of html5_qrcode
+    },
+    html5_qrcode_stop: function() {
+      console.log("i hope not");
+      return this.each(function() {
+        //stop the stream and cancel timeouts
+        $(this).data('stream').getVideoTracks().forEach(function(videoTrack) {
+            videoTrack.stop();
+        });
+
+        clearInterval($(this).data('timeout'));
+      });
+    }
+  });
 })(jQuery);
